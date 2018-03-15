97d8370313e04a1ef480af0f88529126e7dcc60c
Rahul <rramesh@nobul.com>
2018-03-15 14:10:03 -0400

    Shows start new game or the other options depending on whether or not game is in progress

--- app.js
+++ app.js
@@ -5,45 +5,10 @@ new Vue({
     },
     methods: {
         switchGameModeToTrue: function() {
-            gameInProgress = true;
+            this.gameInProgress = true;
         },
         switchGameModeToFalse: function() {
-            gameInProgress = false;
+            this.gameInProgress = false;
         },
     }
 })
-
-// new Vue({
-//     el: '#start-game',
-//     methods: {
-        
-//     }
-// })
-
-// new Vue({
-//     el: '#attack',
-//     methods: {
-        
-//     }
-// })
-
-// new Vue({
-//     el: '#special-attack',
-//     methods: {
-        
-//     }
-// })
-
-// new Vue({
-//     el: '#heal',
-//     methods: {
-        
-//     }
-// })
-
-// new Vue({
-//     el: '#give-up',
-//     methods: {
-        
-//     }
-// })
