6f0419d1d02e90b3326e878db2bd2c7c35718b97
Rahul <rramesh@nobul.com>
2018-03-15 15:39:24 -0400

    Attacks from user and monster

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ new Vue({
     el: '#app',
     data: {
         gameInProgress: false,
+        userAttackScores: [],
+        monsterAttackScores: [],
     },
     methods: {
-        switchGameModeToTrue: function() {
-            this.gameInProgress = true;
+        switchGameMode: function() {
+            if(this.gameInProgress === false) {
+                this.gameInProgress = true;
+            } else {
+                this.gameInProgress = false;
+            }
         },
-        switchGameModeToFalse: function() {
-            this.gameInProgress = false;
-        },
+        
+        handleAttackClick: function() {
+            this.userAttackScores.push(Math.floor((Math.random() * 5) + 1))
+            this.monsterAttackScores.push(Math.floor((Math.random() * 5) + 1))
+            console.log(this.userAttackScores, this.monsterAttackScores)
+        }
     }
 })
